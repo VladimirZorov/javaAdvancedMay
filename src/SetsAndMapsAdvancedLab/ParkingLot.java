d86f7e7a20aab096a835bb3bb6fe09009c59d206
Vladimir Zorov <vl.zorov@gmail.com>
2022-08-26 21:43:03 +0300

    Add

---
+++ src/SetsAndMapsAdvancedLab/ParkingLot.java
@@ -0,0 +1,10 @@
+package SetsAndMapsAdvancedLab;
+
+import java.util.Scanner;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
