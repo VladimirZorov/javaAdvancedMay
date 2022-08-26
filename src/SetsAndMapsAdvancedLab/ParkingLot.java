83a0fa93e7ea8e4bc30be1fdf0f18a14c3e08722
Vladimir Zorov <vl.zorov@gmail.com>
2022-08-26 21:51:59 +0300

    Add

--- src/SetsAndMapsAdvancedLab/ParkingLot.java
+++ src/SetsAndMapsAdvancedLab/ParkingLot.java
@@ -1,10 +1,28 @@
 package SetsAndMapsAdvancedLab;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 public class ParkingLot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        LinkedHashSet<String> parkingLot = new LinkedHashSet<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("END")) {
+            String [] command = input.split(", ");
+            if (command[0].equals("IN")) {
+                parkingLot.add(command[1]);
+            } else if (command[0].equals("OUT")) {
+                parkingLot.remove(command[1]);
+            }
+            input = scanner.nextLine();
+        }
+        if (parkingLot.isEmpty()) {
+            System.out.println("Parking Lot is Empty");
+        }else {
+            parkingLot.forEach(System.out::println);
+        }
     }
 }
