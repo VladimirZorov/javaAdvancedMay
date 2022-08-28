cfcf141928702406af6001bdbf460213b3e32f3b
Vladimir Zorov <vl.zorov@gmail.com>
2022-08-28 18:07:31 +0300

    Add

--- src/SetsAndMapsAdvancedLab/SoftUniParty.java
+++ src/SetsAndMapsAdvancedLab/SoftUniParty.java
@@ -1,7 +1,10 @@
 package SetsAndMapsAdvancedLab;
 
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SoftUniParty {
     public static void main(String[] args) {
@@ -10,6 +13,7 @@ public class SoftUniParty {
         LinkedHashSet<String> guests = new LinkedHashSet<>();
         LinkedHashSet<String> guestList = new LinkedHashSet<>();
         LinkedHashSet<String> dontComeGuest = new LinkedHashSet<>();
+        LinkedHashSet<String> dontComeGuestVip = new LinkedHashSet<>();
 
 
         while (!input.equals("PARTY")) {
@@ -23,12 +27,17 @@ public class SoftUniParty {
 
         for(String guest : guests){
             if (!guestList.contains(guest)) {
-                dontComeGuest.add(guest);
+                if ((guest.charAt(0) != 0)) {
+                    dontComeGuest.add(guest);
+                } else {
+                    dontComeGuestVip.add(guest);
+                }
             }
         }
 
 
         System.out.println(dontComeGuest.size());
+        dontComeGuestVip.forEach(System.out::println);
         dontComeGuest.forEach(System.out::println);
     }
 }
