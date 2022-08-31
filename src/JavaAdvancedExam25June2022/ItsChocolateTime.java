ed20d4b7ef500a1aace2cb906c6103bbeef34b11
Vladimir Zorov <vl.zorov@gmail.com>
2022-08-31 23:29:20 +0300

    Add

--- src/JavaAdvancedExam25June2022/ItsChocolateTime.java
+++ src/JavaAdvancedExam25June2022/ItsChocolateTime.java
@@ -1,8 +1,6 @@
 package JavaAdvancedExam25June2022;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class ItsChocolateTime {
     public static void main(String[] args) {
@@ -11,9 +9,9 @@ public class ItsChocolateTime {
         ArrayDeque<Double> milk = new ArrayDeque<>();
         ArrayDeque<Double> cacao = new ArrayDeque<>();
 
-        double [] milkValue = Arrays.stream(scanner.nextLine().split(" "))
+        double[] milkValue = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToDouble(Double::parseDouble).toArray();
-        double [] cacaoValue = Arrays.stream(scanner.nextLine().split(" "))
+        double[] cacaoValue = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToDouble(Double::parseDouble).toArray();
 
         for (Double milkV : milkValue) {
@@ -24,6 +22,50 @@ public class ItsChocolateTime {
             cacao.push(cacaoV);
         }
 
-        System.out.println();
+        Map<String, Integer> chocolates = new TreeMap<>();
+        chocolates.put("Milk Chocolate", 0);
+        chocolates.put("Dark Chocolate", 0);
+        chocolates.put("Baking Chocolate", 0);
+
+        while (!milk.isEmpty() && !cacao.isEmpty()) {
+            double milkPercentage = milk.peek();
+            double cacaoPercentage = (cacao.peek() / (cacao.peek() + milk.peek()) * 100);
+            switch ((int) cacaoPercentage) {
+                case 30:
+                    chocolates.computeIfPresent("Milk Chocolate", (k, v) -> v + 1);
+                    milk.pop();
+                    cacao.poll();
+                    break;
+                case 50:
+                    chocolates.computeIfPresent("Dark Chocolate", (k, v) -> v + 1);
+                    milk.pop();
+                    cacao.poll();
+                    break;
+                case 100:
+                    chocolates.computeIfPresent("Baking Chocolate", (k, v) -> v + 1);
+                    milk.pop();
+                    cacao.poll();
+                    break;
+                default:
+                    cacao.poll();
+                    milkPercentage+=10;
+                    milk.pop();
+                    milk.addLast(milkPercentage);
+                    break;
+            }
+
+        }
+        if (!chocolates.containsValue(0)) {
+            System.out.println("It’s a Chocolate Time. All chocolate types are prepared.");
+            chocolates.entrySet().forEach(entry -> {
+                System.out.println("# " + entry.getKey() + " --> " + entry.getValue());
+            });
+        } else {
+            System.out.println("Sorry, but you didn't succeed to prepare all types of chocolates.");
+            chocolates.entrySet().forEach(entry -> {
+                if (entry.getValue()!=0)
+                System.out.println("# " + entry.getKey() + " --> " + entry.getValue());
+            });
+        }
     }
 }
