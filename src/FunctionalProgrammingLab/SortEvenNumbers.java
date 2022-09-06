cab37dc247fb317d043cf1b87b2afd8a087ef72a
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-06 19:19:28 +0300

    Add

--- src/FunctionalProgrammingLab/SortEvenNumbers.java
+++ src/FunctionalProgrammingLab/SortEvenNumbers.java
@@ -1,21 +1,20 @@
 package FunctionalProgrammingLab;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SortEvenNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] integers = Arrays.stream(scanner.nextLine().split(", "))
-                .mapToInt(Integer::parseInt).toArray();
+        int[] numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).filter(n -> n % 2 == 0).toArray();
+        System.out.println(formatArray(numbers));
+        Arrays.sort(numbers);
+        System.out.println(formatArray(numbers));
 
-        for (int inter: integers) {
-            if (inter%2==0) {
-                System.out.print(inter + ", ");
-            }
-        }
-
-        System.out.println();
+    }
+    static String formatArray(int[] array) {
+        return Arrays.stream(array).mapToObj(String::valueOf).collect(Collectors.joining(", "));
     }
 }
