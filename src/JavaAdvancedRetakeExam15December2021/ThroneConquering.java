bd2433d003f9f4689cf7c3a0542a988e60efbd90
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-09 20:18:15 +0300

    Add

---
+++ src/JavaAdvancedRetakeExam15December2021/ThroneConquering.java
@@ -0,0 +1,16 @@
+package JavaAdvancedRetakeExam15December2021;
+
+import java.util.Scanner;
+
+public class ThroneConquering {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int energy = Integer.parseInt(scanner.nextLine());
+        int numOfRows = Integer.parseInt(scanner.nextLine());
+
+        String [][] sparta = new String[][];
+
+
+    }
+}
