4f0c100dfae104a72a784a461fed5d549ad1d4f8
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-12 19:39:35 +0300

    Add

--- src/JavaAdvancedRetakeExam18August2021/Pastryshop.java
+++ src/JavaAdvancedRetakeExam18August2021/Pastryshop.java
@@ -6,9 +6,9 @@ public class Pastryshop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int [] liquids = Arrays.stream(scanner.nextLine().split(" "))
+        int[] liquids = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToInt(Integer::parseInt).toArray();
-        int [] ingredients = Arrays.stream(scanner.nextLine().split(" "))
+        int[] ingredients = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToInt(Integer::parseInt).toArray();
 
         ArrayDeque<Integer> liquidSteack = new ArrayDeque<>();
@@ -17,7 +17,7 @@ public class Pastryshop {
         for (int liq : liquids) {
             liquidSteack.push(liq);
         }
-        for (int ing : ingredients){
+        for (int ing : ingredients) {
             ingredientQueue.offer(ing);
         }
 
@@ -28,11 +28,42 @@ public class Pastryshop {
         food.put("Pastry", 0);
         food.put("Pie", 0);
 
-        while (!liquidSteack.isEmpty() && !ingredientQueue.isEmpty()){
+        while (!liquidSteack.isEmpty() && !ingredientQueue.isEmpty()) {
             int liquidToCook = liquidSteack.peek();
             int ingredientToCook = ingredientQueue.peek();
 
-            switch ()
+            int valueNeeded = liquidToCook + ingredientToCook;
+
+            switch (valueNeeded) {
+                case 25:
+                    food.computeIfPresent("Biscuit", (k, v) -> v + 1);
+                    liquidSteack.pop();
+                    ingredientQueue.poll();
+                    break;
+                case 50:
+                    food.computeIfPresent("Cake", (k, v) -> v + 1);
+                    liquidSteack.pop();
+                    ingredientQueue.poll();
+                    break;
+                case 75:
+                    food.computeIfPresent("Pastry", (k, v) -> v + 1);
+                    liquidSteack.pop();
+                    ingredientQueue.poll();
+                    break;
+                case 100:
+                    food.computeIfPresent("Pie", (k, v) -> v + 1);
+                    liquidSteack.pop();
+                    ingredientQueue.poll();
+                    break;
+                default:
+                    liquidSteack.pop();
+                    ingredientQueue.poll();
+                    ingredientQueue.addFirst(ingredientToCook+3);
+            }
         }
+
+
+        food.forEach((s, integer) -> integer.compareTo(0));
+        System.out.println("Great! You succeeded in cooking all the food!");
     }
 }
