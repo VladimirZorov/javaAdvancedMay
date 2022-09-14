90bc2bf7308eb045fcfcb248be193f1e5916df54
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-14 22:20:58 +0300

    Add

--- src/JavaAdvancedRetakeExam18August2021/FormulaOne.java
+++ src/JavaAdvancedRetakeExam18August2021/FormulaOne.java
@@ -1,4 +1,10 @@
 package JavaAdvancedRetakeExam18August2021;
 
+import java.util.Scanner;
+
 public class FormulaOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
