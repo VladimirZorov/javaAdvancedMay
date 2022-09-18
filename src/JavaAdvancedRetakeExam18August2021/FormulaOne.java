baa705617b6dcae5f2a1d67a12db26100d279465
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-18 17:18:27 +0300

    Add

--- src/JavaAdvancedRetakeExam18August2021/FormulaOne.java
+++ src/JavaAdvancedRetakeExam18August2021/FormulaOne.java
@@ -1,10 +1,14 @@
 package JavaAdvancedRetakeExam18August2021;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class FormulaOne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        int size = Integer.parseInt(scanner.nextLine());
+
+
     }
 }
