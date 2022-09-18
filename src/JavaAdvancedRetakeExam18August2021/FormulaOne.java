06505b1e9ca731c5d96c806b57d15dafee2ef673
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-18 21:35:25 +0300

    Add

--- src/JavaAdvancedRetakeExam18August2021/FormulaOne.java
+++ src/JavaAdvancedRetakeExam18August2021/FormulaOne.java
@@ -1,6 +1,5 @@
 package JavaAdvancedRetakeExam18August2021;
 
-import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class FormulaOne {
@@ -26,45 +25,133 @@ public class FormulaOne {
                 }
             }
         }
+        boolean finish = false;
 
-        for (int i = 0; i < countOfCommands; i++) {
-            String command = scanner.nextLine();
-            switch (command) {
-                case "up":
-                    if (row - 1 >= 0) {
-                        row--;
-                    }
-                    break;
-                case "down":
-                    if (row+1 < size){
-                        row++;
-                    }
-                    break;
-                case "left":
-                    if (coll-1 >=0){
-                        coll--;
-                    }
-                    break;
-                case "right":
-                    if (coll+1 <size){
-                        coll ++;
-                    }
-                    break;
+            for (int i = 0; i < countOfCommands; i++) {
+                if (!finish) {
+                String command = scanner.nextLine();
+                switch (command) {
+                    case "up":
+                        field [row][coll] = ".";
+                        if (row - 1 >= 0) {
+                            row--;
+                            field [row][coll] = ".";
+                            if (field[row][coll].equals("B")) {
+                                row--;
+                            } else if (field[row][coll].equals("T")) {
+                                row++;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        } else {
+                            row = size - 1;
+                            if (field[row][coll].equals("B")) {
+                                row--;
+                            } else if (field[row][coll].equals("T")) {
+                                row++;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        }
+                        break;
+                    case "down":
+                        field [row][coll] = ".";
+                        if (row + 1 < size) {
+                            row++;
+                            if (field[row][coll].equals("B")) {
+                                row++;
+                            } else if (field[row][coll].equals("T")) {
+                                row--;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        } else {
+                            row = 0;
+                            if (field[row][coll].equals("B")) {
+                                row++;
+                            } else if (field[row][coll].equals("T")) {
+                                row--;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        }
+                        break;
+                    case "left":
+                        field [row][coll] = ".";
+                        if (coll - 1 >= 0) {
+                            coll--;
+                            if (field[row][coll].equals("B")) {
+                                coll--;
+                            } else if (field[row][coll].equals("T")) {
+                                coll++;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        } else {
+                            coll = size - 1;
+                            if (field[row][coll].equals("B")) {
+                                coll--;
+                            } else if (field[row][coll].equals("T")) {
+                                coll++;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        }
+                        break;
+                    case "right":
+                        field [row][coll] = ".";
+                        if (coll + 1 < size) {
+                            coll++;
+                            if (field[row][coll].equals("B")) {
+                                coll++;
+                            } else if (field[row][coll].equals("T")) {
+                                coll--;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        } else {
+                            coll = 0;
+                            if (field[row][coll].equals("B")) {
+                                coll++;
+                            } else if (field[row][coll].equals("T")) {
+                                coll--;
+                            } else if (field[row][coll].equals("F")) {
+                                finish = true;
+                            }
+                            field[row][coll] = "P";
+                        }
+                        break;
+                }
+
+        }else {
+                    System.out.println("Well done, the player won first place!");
+                    printField(size, field);
+                }
             }
-            if (field[row][coll].equals("F")){
+            if (finish) {
                 System.out.println("Well done, the player won first place!");
-                printfield();
-                break;
-            } else if (field[row][coll].equals("T")) {
-
-            } else if (field[row][coll].equals("B"))
+                printField(size,field);
+            } else {
+                System.out.println("Oh no, the player got lost on the track!");
+                printField(size, field);
+            }
 
         }
-        System.out.println();
+
+
+    private static void printField(int size, String [][]field) {
+        for (int j = 0; j < size; j++) {
+            for (int k = 0; k < size; k++) {
+                System.out.print(field[j][k]);
+            }
+            System.out.println();
+        }
     }
-
-    private static void printfield() {
-
-    }
-
 }
