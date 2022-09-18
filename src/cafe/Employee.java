fff782be502ff2e5ba1f7cb41ca4a1c9e149d4f7
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-18 21:51:28 +0300

    Add

---
+++ src/cafe/Employee.java
@@ -0,0 +1,19 @@
+package cafe;
+
+public class Employee {
+
+    public String name;
+    public int age;
+    public String country;
+
+    public Employee(String name, int age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee: %s, %d from %s", this.name, this.age, this.country);
+    }
+}
